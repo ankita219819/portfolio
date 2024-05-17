7b58d776df9e51658249c40e7925d3bd10ca3d8e
ankita219819 <ankiii.gupta@gmail.com>
2024-05-17 20:14:26 +0530

    added project section in mobile view

--- src/components/mobile-navbar.js
+++ src/components/mobile-navbar.js
@@ -4,7 +4,7 @@ import "../css/mobile-navbar.css";
 const MobileNavbar = () => {
     const [activeNav, setActiveNav] = useState('#')
     return (
-        <div class="nav">
+        <div className="nav">
             <a href="#projects" onClick={() => setActiveNav('#')} className={`m-nav ${activeNav === '#' ? 'active' : ''}`}>
                 <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                     <path d="m12.223 11.641-.223.22-.224-.22a2.224 2.224 0 0 0-3.125 0 2.13 2.13 0 0 0 0 3.07L12 18l3.349-3.289a2.13 2.13 0 0 0 0-3.07 2.225 2.225 0 0 0-3.126 0z"></path
